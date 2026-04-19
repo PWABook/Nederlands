89af3d68f23a3b1eae205d924483d90e5aa8e9e4
PWABook <lvolders+pwabook@gmail.com>
2026-04-19 15:00:06 +0200

    Add files via upload
    De bestanden voor Hoofdstuk 17.
    Hier wordt een web-app gecreerd met een Installatie knop.

---
+++ Hoofdstuk-17/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "nw-klok-v4";
+const APP_SHELL = [
+    "/",
+"/index.html",
+"/manifest.json",
+"/icon-192.png",
+"/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+        Promise.all(
+            keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+        )
+        )
+    );
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(cached => {
+            return cached || fetch(event.request);
+        })
+    );
+});
+
